94e763601fff9cdbe36525a297dac1f7f9b26d8a
herb <inwooha00@naver.com>
2022-05-01 01:00:15 +0900

    Update studycommunity.js

--- routes/studycommunity.js
+++ routes/studycommunity.js
@@ -3,25 +3,40 @@ const fs = require('fs')
 const router = express.Router()
 const decrypt = require('../modules/decrypt')
 const encrypt = require('../modules/encrypt')
+let viewlist
+function refrashdata() {
+    fs.readFile('/static/data/communitydata.json',(err,data) =>{
+        if(data == undefined){
+            viewlist = {}
+        }else{
+            decrypt(data,(data_) => {
+                viewlist = data_
+            })
+        }
+    })
+}
 
 router.get('/',async (req,res) => {
     if (req.session.key == undefined){
         res.send('<script>alert("카카오 로그인이 필요합니다");window.location = "/kakologin/authorize"</script>')
     }
-    fs.readFile('/static/data/communitydata.json',(err,data) =>{
-        if(data == undefined){
-          console.log(data)
-            res.render('community',{views : new Array()})
-        }else{
-            decrypt(data,(data_) => {
-                res.render("community.ejs",{views : Object.entries(data_)})
-            })
-        }
-    })
+    res.render("community",{views : Object.entries(viewlist)})
+    
 })
 
 router.get('/form', async (req,res) => {
-    res.render('communityform')
+    if (req.session.key) {
+        var islogin = true
+    }
+    res.render('communityform',{islogin:islogin})
+})
+
+router.get('/:id',async (req,res) => {
+    
+    if (req.session.key) {
+        var islogin = true
+    }
+    res.render('view',{islogin:islogin,view:viewlist[req.params.id]})
 })
 
 router.post('/upload',async (req,res) => {
