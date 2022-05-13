2cedfcc1c4c88e60896d595371c01da3f9b5c9a4
herb <69900670+WhiteHerb@users.noreply.github.com>
2022-05-13 14:39:38 +0000

    add tips

--- routes/studygroup.js
+++ routes/studygroup.js
@@ -2,11 +2,21 @@ const express = require('express')
 const router = express.Router()
 const fs = require('fs')
 const studycommunity = require('./studycommunity') 
+const data = require('')
 
 router.get("/tip",async (req,res) => {
     fs.readFile("./static/data/studytips.json", (err,data) => {
         const tipdata = JSON.parse(data)
-        res.render("StudyTip.ejs",{tips:tipdata})
+      const request = require('request'); // POST 요청하기 http://www.google.com
+      const options = { 
+        uri:'https://nel951.kro.kr/Dday', 
+        method: 'GET',
+        json:true 
+      }
+        request.get(options, function (error, response, body) {
+          console.log(body)
+          res.render("StudyTip.ejs",{tips:tipdata,text:body.text,writer:body.writer})
+        })
     })
 })
 
